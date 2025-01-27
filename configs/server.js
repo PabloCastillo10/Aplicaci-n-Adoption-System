f5494ab5fdf1d8e42a081dd1b1fd33579ca4ae50
Informatica <informatica@alumno.local>
2025-01-27 11:57:03 -0600

    creacion servidor

---
+++ configs/server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import {dbConecction} from './mongo.js';
+
+const configurarMiddlewares = (app) => {
+    app.use(express.urlencoded({extended: false}));
+    app.use(cors());
+    app.use(express.json());
+    app.use(helmet());
+    app.use(morgan('dev'));
+}
+
+const configurarRutas = () => {
+
+}
+
+ const conectarDB = async  () => {
+    try{
+        await dbConecction();
+        console.log("Conexión a la base de datos exitosa");
+    }catch(error){
+        console.error('Error conectando a la base de datos', error);
+        process.exit(1);
+    }
+}
+
+export const iniciarServidor = async () => {
+    const app = express();
+    const port = process.env.PORT || 3000;
+
+    await conectarDB();
+
+    configurarMiddlewares(app);
+    configurarRutas(app);
+
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
